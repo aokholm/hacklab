96a138887c18d271f55a020f044a0a96bdcb5c67
Andreas Bruun Okholm <aokholm@gmail.com>
2014-03-10 14:56:01 +0100

    Arduinoi SoftModem Sketches

---
+++ Arduino/SoftModemT2/SoftModemT2/SoftModemT2.ino
@@ -0,0 +1,27 @@
+#include "SoftModem.h"
+#include "ctype.h"
+SoftModem modem;
+
+int ledPin = 13; // choose the pin for the LED
+int inPin = 7;   // choose the input pin (for a pushbutton)
+int val = 0;     // variable for reading the pin status
+
+void setup() {
+  pinMode(ledPin, OUTPUT);  // declare LED as output
+  pinMode(inPin, INPUT);    // declare pushbutton as input
+  modem.begin();
+}
+
+void loop(){
+  val = digitalRead(inPin);  // read input value
+  if (val == HIGH) {         // check if the input is HIGH (button released)
+    digitalWrite(ledPin, LOW);  // turn LED OFF
+  } else {
+     // We have a heart beat:
+     char h = '1';
+     //modem.write(h);
+     digitalWrite(ledPin, HIGH);
+     delay(100);
+     digitalWrite(ledPin, LOW);
+  }
+}
